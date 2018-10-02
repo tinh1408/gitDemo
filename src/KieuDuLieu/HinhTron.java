36ce5e4418d8fe795042c769e51b7d1d8cc352dc
tinh1408 <tinhph@hotmail.com>
2018-10-02 15:32:01 +0700

    A Tính góp ý

--- src/KieuDuLieu/HinhTron.java
+++ src/KieuDuLieu/HinhTron.java
@@ -6,8 +6,8 @@ public class HinhTron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap ban kinh r:");
-        int r = scanner.nextInt();
-        double chuVi = r*2*3.14;
+        int r = scanner.nextInt(); // tên biến cần rõ ràng hơn ví dụ: banKinh
+        double chuVi = r*2*3.14; // 3.14 là hằng số em nên khai báo là: final int PI = 3.14; nhé
         System.out.println("chu vi hinh tron :"+chuVi);
         double dienTich = r*r*3.14;
         System.out.println("dien tich hinh tron:"+dienTich);
