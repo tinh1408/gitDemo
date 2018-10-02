fde64a09735d7b956c7cb8e6016b88f1a726d6be
tinh1408 <tinhph@hotmail.com>
2018-10-02 15:31:33 +0700

    A

--- src/KieuDuLieu/HinhTron.java
+++ src/KieuDuLieu/HinhTron.java
@@ -7,7 +7,7 @@ public class HinhTron {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap ban kinh r:");
         int r = scanner.nextInt();
-        double chuVi = r*2*3.14;
+        double chuVi = r*2*3.14; // 3.14 là hằng số em nên khai báo là: final int PI = 3.14; nhé
         System.out.println("chu vi hinh tron :"+chuVi);
         double dienTich = r*r*3.14;
         System.out.println("dien tich hinh tron:"+dienTich);
